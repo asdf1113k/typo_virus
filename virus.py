4a418333bda44713314575cefb17fd1eb704b73a
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-04-02 14:25:12 +0300

    change [91]: 'set' instead of 'list' add [97-102: new paths

--- virus.py
+++ virus.py
@@ -88,14 +88,19 @@ class Virus:
 "HellHammer — адский молот"
 ]
 
-    list_url_www_jobs_org:list[str] = [
+    set_url_www_jobs_org:set[str] = set([
 '',
 '100/index.html',
 'id.html',
 'indexx.html',
 '100/00.html',
-'',
-]
+'../id.html',
+'hqx/index.html',
+'../beta/untitled/4.html',
+'../goodtimes/index.html',
+'../beta/index.html',
+'hqx/i802.html',
+])
 
     @staticmethod
     def run():
