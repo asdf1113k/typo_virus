7744f39a69609e018dfecf450353089fc0b5a184
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-04-02 16:40:47 +0300

    test: function set()

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-import os
+set_url_www_jobs_org:list[str] = set([
+'',
+'100/index.html',
+'id.html',
+'indexx.html',
+'100/00.html',
+'../id.html',
+'hqx/index.html',
+'../beta/untitled/4.html',
+'../goodtimes/index.html',
+'../beta/index.html',
+'hqx/i802.html',
+])
 
-os.system("cmd /c")
-
+print(list_url_www_jobs_org)
