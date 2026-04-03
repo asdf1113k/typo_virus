7b7a2b3377c077b0079cdc2a29343ae0b8f8d38b
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-04-03 15:17:51 +0300

    тестировал прогресс бар

--- test.py
+++ test.py
@@ -1,15 +1,5 @@
-set_url_www_jobs_org:list[str] = set([
-'',
-'100/index.html',
-'id.html',
-'indexx.html',
-'100/00.html',
-'../id.html',
-'hqx/index.html',
-'../beta/untitled/4.html',
-'../goodtimes/index.html',
-'../beta/index.html',
-'hqx/i802.html',
-])
-
-print(list_url_www_jobs_org)
+# TEST PROGRES BAR
+from alive_progress import alive_bar
+# import time
+with alive_bar(3) as bar:
+    bar()
