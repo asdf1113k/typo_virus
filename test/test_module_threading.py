f1647542aca0b1625bced61023ce382f1a7b329b
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-05-16 15:25:57 +0300

    сделал первые тесты модуля threading скорее всего перейду на асинхронный вариан а не много поточный

---
+++ test/test_module_threading.py
@@ -0,0 +1,25 @@
+import threading
+
+from time import sleep
+
+
+
+func1 = lambda :print(["отработала 1", sleep(5)])
+func2 = lambda :print(["отработала 1", sleep(5)])
+func3 = lambda :print(["отработала 1", sleep(5)])
+
+# func1()
+# func2()
+# func3()
+
+thread1 = threading.Thread(target=func3)
+thread2 = threading.Thread(target=func1)
+thread3 = threading.Thread(target=func2)
+
+thread1.start()
+thread2.start()
+thread3.start()
+
+thread1.join()
+thread2.join()
+thread3.join()
